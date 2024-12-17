c32188510f4ebae090b29bc39d34087d2d018a92
Ju Won <juwonpee@gmail.com>
2024-12-17 15:25:41 +0900

    Vector double precision 2,3,4
    Quaternion double WIP

---
+++ Quaterniond.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace UnityEngine {
+
+    [Serializable] public struct Quaterniond
+    {
+        public double w,x,y,z;
+
+        public Quaterniond(Vector4d v4) {
+            this.w = v4.w;
+            this.x = v4.x;
+            this.y = v4.y;
+            this.z = v4.z;
+        }
+
+        public Quaterniond(double x, double y, double z, double w) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public static Quaterniond identity = new Quaterniond(0d, 0d, 0d, 1d);
+        
+        public double this[int index] {
+            get {
+                return index switch {
+                    0 => w,
+                    1 => x,
+                    2 => y,
+                    3 => z,
+                    _ => throw new IndexOutOfRangeException("Invalid Quaterniond index!"),
+                };
+                
+            }
+            set {
+                switch (index) {
+                    case 0:
+                        this.w = value;
+                        break;
+                    case 1:
+                        this.x = value;
+                        break;
+                    case 2:
+                        this.y = value;
+                        break;
+                    case 3:
+                        this.z = value;
+                        break;
+                    default:
+                        throw new IndexOutOfRangeException("Invalid Quaterniond index!");
+                }
+            }
+        }
+
+        public Quaterniond normalized {
+            get {
+                double magnitude = Math.Sqrt(x*x + y*y + z*z + w*w);
+                return new Quaterniond (
+                    this.x / magnitude,
+                    this.y / magnitude,
+                    this.z / magnitude,
+                    this.w / magnitude
+                );
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Double-Precision-Vectors
+Extension of this repository.
+https://raw.githubusercontent.com/sldsmkd/vector3d
+
